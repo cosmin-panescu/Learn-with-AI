95ca17e1245ba2bcf2a5c7e587ede3a12fbce5e4
Cosmin <1panescu.cosmin@gmail.com>
2024-05-01 23:27:39 +0300

    change the port

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import "dotenv/config";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5050;
 app.use(bodyParser.json());
 app.use(cors());
 
