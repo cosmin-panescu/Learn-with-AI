8f3d6c25439283950343bdc869b98863a02ae26e
Cosmin <1panescu.cosmin@gmail.com>
2024-05-13 23:31:50 +0300

    Add 'not found 404' page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Quiz from "./pages/Quiz/Quiz";
 import AISimulator from "./pages/AISimulator/AISimulator";
-import { Route, Routes, HashRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound/NotFound";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route path="/test/:tehnologie" element={<Quiz />} />
           <Route path="/simulare-AI" element={<AISimulator />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
     </>
